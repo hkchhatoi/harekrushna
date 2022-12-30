4c83f6952952c3a6b79773504c45d929a732cb71
hkchhatoi <112386487+hkchhatoi@users.noreply.github.com>
2022-12-30 18:28:39 +0530

    Update hk.js

--- hk.js
+++ hk.js
@@ -7,7 +7,7 @@ const num3 = parseFloat(prompt("Enter third number: "));
 let largest;
 
 // check the condition
-if(num1 >= num2 && num1 >= num3) {
+if(num1 >= num2 && num1 >= num3) {{
     largest = num1;
 }
 else if (num2 >= num1 && num2 >= num3) {
@@ -40,3 +40,11 @@ els {
 
 // display the result
 console.log("The largest number is " + largest);
+                                  else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+                                      else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+                                          else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+                                              else if (num2 >= num1 && num2 >= num3) {{
+    largest = num2;
