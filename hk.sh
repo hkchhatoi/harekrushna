b46184c6de41843454f9181d2cecd12204c522e7
hkchhatoi <112386487+hkchhatoi@users.noreply.github.com>
2022-12-30 17:39:19 +0530

    Update hk.sh

--- hk.sh
+++ hk.sh
@@ -1,6 +1,11 @@
 example of using arguments to a script
 echo "My first name is rrr1"
 echoo "today is 30th aug 2022zzzz"
+echoo "today is 30th aug 2022zzzz"
+echoo "today is 30th aug 2022zzzz"
+echoo "today is 30th aug 2022zzzz"
+echoo "today is 30th aug 2022zzzz"
+echoo "today is 30th aug 2022zzzz"
 if{
 
 
