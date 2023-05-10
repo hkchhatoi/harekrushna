9b9f32a398b6030e0a9d047549ab0f775adc5708
hkchhatoi <krushna.cht@gmail.com>
2023-05-10 23:46:17 +0530

    hkjkk

--- hk.sh
+++ hk.sh
@@ -1,6 +1,6 @@
 
 echo "My first name is 1"
-echoo "today is 30th aug 325q3253252353252511111111111111"
+echoo "today is 30th aug 325q3253252353252511111111111111sadasdads"
 =======
 echo "My first name is rrr1"
 echoo "today is 30th aug 2022zzzz"
